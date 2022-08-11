62e00d268d2c667f0cd79233cba50b1305a700e0
Angelo <angelohague@aol.co.uk>
2022-08-11 17:34:51 +0100

    First Commit. Template info and nav design

---
+++ assets/application.js
@@ -0,0 +1,15 @@
+
+let navOpen = false;
+
+function toggleNav(element) {
+  let nav = document.getElementById("nav");
+    if (!navOpen) {
+      element.classList.add('open');
+      nav.className = 'active';
+      navOpen = true;
+    } else {
+      element.classList.remove('open');
+      nav.className = '';
+      navOpen = false;
+    }
+}
