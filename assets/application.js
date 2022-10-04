6c5291ab580fa4ba11ac75d062258c66584d64a2
Angelo <angelohague@aol.co.uk>
2022-10-04 23:17:52 +0100

    Completed CSS only mobile navigation

--- assets/application.js
+++ assets/application.js
@@ -1,15 +1,15 @@
 document.documentElement.className = "js"
-let navOpen = false;
+// let navOpen = false;
 
-function toggleNav(element) {
-  let nav = document.getElementById("nav");
-    if (!navOpen) {
-      element.classList.add('open');
-      nav.className = 'active';
-      navOpen = true;
-    } else {
-      element.classList.remove('open');
-      nav.className = '';
-      navOpen = false;
-    }
-}
+// function toggleNav(element) {
+//   let nav = document.getElementById("nav");
+//     if (!navOpen) {
+//       element.classList.add('open');
+//       nav.className = 'active';
+//       navOpen = true;
+//     } else {
+//       element.classList.remove('open');
+//       nav.className = '';
+//       navOpen = false;
+//     }
+// }
